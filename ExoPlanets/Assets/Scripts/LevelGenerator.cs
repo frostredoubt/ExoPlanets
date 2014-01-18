5451a6da475377897c6327d619a5f9b9d24ff18f
thefil <filipkrynicki@gmail.com>
2014-01-18 12:50:59 -0500

    Level generation algorithm with guaranteed path

--- ExoPlanets/Assets/Scripts/LevelGenerator.cs
+++ ExoPlanets/Assets/Scripts/LevelGenerator.cs
@@ -10,19 +10,17 @@ public class LevelGenerator : MonoBehaviour
     #region Member Variables
 
     /// <summary>
-    /// TODO: Temporary tile size. This should be programmatically determined according to screen size / asset size.
-    /// </summary>
-    public const int TILE_SIZE = 64;
-
-    /// <summary>
     /// The maximum allowable width for a map in tiles.
     /// </summary>
-    public const int MAX_MAP_WIDTH = 64;
+    public const int ROOM_WIDTH = 16;
 
     /// <summary>
     /// The maximum allowable height for a map in tiles.
     /// </summary>
-    public const int MAX_MAP_HEIGHT = 64;
+    public const int ROOM_HEIGHT = 16;
+
+	public const int LEVEL_ROOMS_X = 4;
+	public const int LEVEL_ROOMS_Y = 4;
 
     /// <summary>
     /// The current width of the map in tiles.
@@ -80,6 +78,8 @@ public class LevelGenerator : MonoBehaviour
     /// </summary>
     public GameObject tileFloor;
 
+	public GameObject levelTypeLabel;
+
     #endregion
 
     /// <summary>
@@ -87,11 +87,11 @@ public class LevelGenerator : MonoBehaviour
     /// </summary>
     private void Awake()
     {
-        tiles = new Tile[MAX_MAP_WIDTH, MAX_MAP_HEIGHT];
-        tileComponents = new GameObject[MAX_MAP_WIDTH, MAX_MAP_HEIGHT];
-        for (int i = 0; i < MAX_MAP_WIDTH; i += 1)
+		tiles = new Tile[LEVEL_ROOMS_X * ROOM_WIDTH, LEVEL_ROOMS_Y * ROOM_HEIGHT];
+		tileComponents = new GameObject[LEVEL_ROOMS_X * ROOM_WIDTH, LEVEL_ROOMS_Y * ROOM_HEIGHT];
+		for (int i = 0; i < LEVEL_ROOMS_X * ROOM_WIDTH; i += 1)
         {
-            for (int j = 0; j < MAX_MAP_HEIGHT; j += 1)
+			for (int j = 0; j < LEVEL_ROOMS_Y * ROOM_HEIGHT; j += 1)
             {
                 tiles[i, j] = new Tile(Tile.TileOption.Empty);
             }
@@ -113,28 +113,110 @@ public class LevelGenerator : MonoBehaviour
     /// </summary>
     private void GenerateLevel()
     {
-        SetDimensions(); // Determine how large the current level is
+        //SetDimensions(); // Determine how large the current level is
 
-        for (int i = 0; i < mapWidth; i += 1)
+		int[,] roomTypes = GenerateRooms (LEVEL_ROOMS_X, LEVEL_ROOMS_Y);
+
+		for (int i = 0; i < LEVEL_ROOMS_X; i += 1)
         {
-            for (int j = 0; j < mapHeight; j += 1)
-            {
-                tiles[i, j].type = Tile.TileOption.Floor;
-                tileComponents[i, j] = Instantiate(tileFloor, new Vector3(i, j), Quaternion.identity) as GameObject;
-                tileComponents[i, j].transform.parent = this.transform;
+			for (int j = 0; j < LEVEL_ROOMS_Y; j += 1)
+			{
+				int base_x =  i * ROOM_WIDTH;
+				int base_y = j * ROOM_HEIGHT;
+				//Debug.Log (roomTypes[i, j].ToString() + " " + base_x.ToString () + " " + base_y.ToString());
+
+				bool has_left_right_exit = roomTypes[i, j] > 0;
+				bool has_top_exit = roomTypes[i, j] == 2;
+				bool has_bottom_exit = roomTypes[i, j] == 3 || (roomTypes[i, j] == 2 && j > 0 && roomTypes[i, j-1] == 2);
+
+				for (int x = 0; x < ROOM_WIDTH; x += 1) {
+					for(int y = 0; y < ROOM_HEIGHT; y += 1) {
+						int full_x = base_x + x;
+						int full_y = base_y + y;
+
+						if( (y == 7 || y == 8) && has_left_right_exit ) {
+							Debug.Log ("happens");
+							tiles[full_x, full_y].type = Tile.TileOption.Floor;
+						} else if ((x == 7 || x == 8) && y > 8 && has_top_exit) {
+							tiles[full_x, full_y].type = Tile.TileOption.Floor;
+						} else if ((x == 7 || x == 8) && y < 7 && has_bottom_exit) {
+							tiles[full_x, full_y].type = Tile.TileOption.Floor;
+						}
+
+						GameObject prefab = tiles[full_x, full_y].type == Tile.TileOption.Floor ? tileFloor : tileEmpty;
+
+						tileComponents[full_x, full_y] = Instantiate(prefab, new Vector3(full_x, full_y), Quaternion.identity) as GameObject;
+						tileComponents[full_x, full_y].transform.parent = this.transform;
+					}
+				}
+
+				// Add a room label for debugging purposes (does not work)
+				/*GameObject label = Instantiate(levelTypeLabel, new Vector3(base_x, base_y), Quaternion.identity) as GameObject;
+				GUIText text = label.GetComponent<GUIText>();
+				text.text = roomTypes[i, j].ToString();
+				text.transform.parent = this.transform;*/
             }
         }
 
         return;
     }
+	
+	/// <summary>
+	/// Generates the rooms.
+	/// 
+	/// Room types:
+	/// 0 - no guarantees
+	/// 1 - guarantees left and right exits
+	/// 2 - guarantees left, right, and up exit. Guarantees down exit if another 2 below it
+	/// 3 - guarantees left, right, and down exit
+	/// 
+	/// 4 - room with an exit
+	/// </summary>
+	private int[,] GenerateRooms(int width, int height)
+	{
+		int[,] roomTypes = new int[width, height];
+		int row = 0;
+		int col = Random.Range (0, width - 1);
+		int? lastType = null;
+		int? lastDir = null;
+
+		while(true) {
+			roomTypes[col, row] = lastType == 2 ? Random.Range(2, 3) : 1;
+			lastType = roomTypes[row, col];
+			int moveDir = Random.Range(1, 5);
+
+			if(moveDir < 3 && col > 0 && (lastDir == null || lastDir < 3 || lastDir > 4)) { // Try and move left
+				col -= 1;
+				lastDir = 1;
+				continue;
+			}
+
+			if(moveDir < 5 && col < width - 1 && (lastDir == null || lastDir > 2)) { // Try and move right
+				col += 1;
+				lastDir = 3;
+				continue;
+			} 
+
+			// Try and move up
+			lastDir = 5;
+			if(row < height - 1) {
+				roomTypes[col, row] = 2;
+				lastType = roomTypes[col, row];
+				row += 1;
+			} else {
+				roomTypes[col, row] = 4;
+				return roomTypes;
+			}
+		}
+	}
 
     /// <summary>
     /// Reset the dimensions for the current map.
     /// </summary>
     private void SetDimensions()
     {
-        mapWidth = MAX_MAP_WIDTH;
-        mapHeight = MAX_MAP_HEIGHT;
+        /*mapWidth = MAX_MAP_WIDTH;
+        mapHeight = MAX_MAP_HEIGHT;*/
         return;
     }
 
