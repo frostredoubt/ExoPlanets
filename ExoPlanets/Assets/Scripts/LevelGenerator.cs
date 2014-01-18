e65ab1d3813973a61ef0a8f0ccfbb9b4da580ad4
becassel <becassel@uwaterloo.ca>
2014-01-18 14:39:53 -0500

    Updated template prefabs to allow exit selection through components

--- ExoPlanets/Assets/Scripts/LevelGenerator.cs
+++ ExoPlanets/Assets/Scripts/LevelGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 /// <summary>
 /// The generator used to create new levels.
--- ExoPlanets/Assets/templateAnnounce.cs
+++ ExoPlanets/Assets/templateAnnounce.cs
@@ -1,17 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class templateAnnounce : MonoBehaviour {
+/// <summary>
+/// An announcement of exits that get attached to a template to push updates to the parent.
+/// </summary>
+public class TemplateAnnounce : MonoBehaviour
+{
 
-	public string supported_exits;
+    /// <summary>
+    /// The supported exit paths out of a template.
+    /// </summary>
+    public enum Exit
+    {
+        TOP_LEFT,
+        TOP_MIDDLE,
+        TOP_RIGHT,
+        RIGHT_TOP,
+        RIGHT_MIDDLE,
+        RIGHT_BOTTOM,
+        BOTTOM_LEFT,
+        BOTTOM_MIDDLE,
+        BOTTOM_RIGHT,
+        LEFT_TOP,
+        LEFT_MIDDLE,
+        LEFT_BOTTOM
+    }
+
+    /// <summary>
+    /// The exits supported by a particular template.
+    /// </summary>
+    public Exit[] SupportedExits;
 
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
