e073af5a51ab3acaf133e5921f6c8c95d6c2a041
Alex Szlavik <aszlavik@uwaterloo.ca>
2014-01-18 03:31:51 -0500

    Tyler

--- ExoPlanets/Assets/Scripts/Move.cs
+++ ExoPlanets/Assets/Scripts/Move.cs
@@ -1,7 +1,10 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
 public class Move : MonoBehaviour {
+
+    public int H_Force      = 80;
+    public int Jump_Force   = 300;
 
 	// Use this for initialization
 	void Start () {
@@ -13,10 +16,12 @@ public class Move : MonoBehaviour {
 	
 	}
 
-	void FixedUpdate(){
-		if (Input.GetButton("Horizontal")) {
-			Debug.Log("Fil is gay");
-		}
-	}
-
+    void FixedUpdate()
+    {
+        float H_Movement = Input.GetAxis("Horizontal");
+        //float V_Movement = Input.GetAxis("Vertical");
+        
+        rigidbody2D.AddForce(new Vector2(H_Movement*H_Force,0));   
+        //rigidbody2D.AddForce(new Vector2(0, Jump_Force));
+    }
 }
