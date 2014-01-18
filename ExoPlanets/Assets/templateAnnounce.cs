c0fcf5daae9d9a12ffeabdedb7505640d150d324
thefil <filipkrynicki@gmail.com>
2014-01-18 14:21:15 -0500

    Added script to announce what exits a template supports

---
+++ ExoPlanets/Assets/templateAnnounce.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class templateAnnounce : MonoBehaviour {
+
+	public string supported_exits;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
