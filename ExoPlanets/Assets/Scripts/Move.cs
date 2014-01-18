1b009d41c67971613289db37aeaf4f4d256689c2
Alex Szlavik <aszlavik@uwaterloo.ca>
2014-01-18 04:33:43 -0500

    Simple jumping

--- ExoPlanets/Assets/Scripts/Move.cs
+++ ExoPlanets/Assets/Scripts/Move.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class Move : MonoBehaviour {
 
     public int H_Force      = 80;
-    public int Jump_Force   = 300;
+    public int Jump_Force   = 1000;
+    
+    private bool jumping = false;
 
 	// Use this for initialization
 	void Start () {
@@ -13,15 +15,29 @@ public class Move : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    bool ground_intersect = Physics2D.Linecast(transform.position, transform.Find("Ground_collider").position, 1 << LayerMask.NameToLayer("Ground") );
+        if( ground_intersect && Input.GetButtonDown("Jump") )
+            jumping = true;
 	}
 
     void FixedUpdate()
     {
         float H_Movement = Input.GetAxis("Horizontal");
-        //float V_Movement = Input.GetAxis("Vertical");
-        
-        rigidbody2D.AddForce(new Vector2(H_Movement*H_Force,0));   
-        //rigidbody2D.AddForce(new Vector2(0, Jump_Force));
+
+        if (H_Movement < 0.1)
+        {
+            if (rigidbody2D.velocity.y == 0)
+            {
+                rigidbody2D.velocity = new Vector2(0, rigidbody2D.velocity.y);
+            }
+        }
+
+        rigidbody2D.AddForce(new Vector2(H_Movement * H_Force, 0));
+
+        if (jumping)
+        {
+            rigidbody2D.AddForce(new Vector2(0, Jump_Force));
+            jumping = false;
+        }
     }
 }
